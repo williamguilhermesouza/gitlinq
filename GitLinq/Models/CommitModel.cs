c98a1891d685b0070340ab73d8b700af4add5db8
williamguilhermesouza <williamguilhermesouza@gmail.com>
2025-12-17 07:34:44 -0300

    cleanup

--- GitLinq/Models/CommitModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GitLinq.Models;
-
-public class CommitModel
-{
-    public required string Id { get; set; }
-    public required string Message { get; set; }
-    public required string AuthorName { get; set; }
-    public DateTimeOffset When { get; set; }
-}
