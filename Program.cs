aacad1f4e8a569105589a8d9928718286cfcf3fd
williamguilhermesouza <williamguilhermesouza@gmail.com>
2025-12-17 20:16:58 -0300

    readline with history

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+ReadLine.HistoryEnabled = true;
+
+while (Prompt(out string input))
+{
+    if (string.IsNullOrEmpty(input))
+        continue;
+
+    if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+        break;
+}
+
+static bool Prompt(out string input)
+{
+    input = ReadLine.Read("gitlinq> ");
+    AnsiConsole.MarkupLine($"[green] typed: [/] {input}");
+    return true;
+}
