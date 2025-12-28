ae95996df21d236ff94ba380fd981c389e5053b4
williamguilhermesouza <williamguilhermesouza@gmail.com>
2025-12-28 14:04:00 -0300

    moving files; creating tests

---
+++ tests/QueryParserTests.cs
@@ -0,0 +1,20 @@
+﻿using GitLinq;
+using GitLinq.AST;
+
+namespace QueryParserTest
+{
+    [TestClass]
+    public sealed class BaseTests
+    {
+        [TestMethod]
+        public void ParseIdentifier()
+        {
+            var identifier = "Commits";
+
+            var root = QueryParser.ParseExpression(identifier);
+
+            Assert.IsNotNull(root);
+            Assert.IsInstanceOfType(root, typeof(IdentifierNode));
+        }
+    }
+}
